3bb9d835a20a24069ed0bbf4466a83688cc60a65
Herman Kwamebour <hermankwamebour30@gmail.com>
2026-02-23 14:22:59 +0000

    feat: implement theme management and update App component styling

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {theme} from './theme';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Herman</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.background,
+    color: theme.colors.text,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#fff',
+  }
 });
